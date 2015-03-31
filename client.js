7ae11a4ca9a8052a2807504d8cd9f25f33900852
Yuriy Grunin <yu.grunin@oorraa.net>
2015-03-31 11:02:50 +0300

    Add `simulateDataFetching` function

--- client.js
+++ client.js
@@ -3,13 +3,18 @@ var Router = require('react-router');
 var A = require('./components/A.jsx');
 var routes = require('./routes.jsx');
 
-var renderHandlerTimeoutId;
+var fetchSimulationTimeoutId;
+function simulateDataFetching(cb) {
+    // Simulation of resetting data fetching for the currently pending route
+    clearTimeout(fetchSimulationTimeoutId);
+    // Simating data fetching for the current route
+    fetchSimulationTimeoutId = setTimeout(cb, 500);
+}
 
 Router.run(routes, Router.HistoryLocation, function (Handler, state) {
     console.log('Matched routes for "' + state.path + '" path');
-    clearTimeout(renderHandlerTimeoutId);
-    renderHandlerTimeoutId = setTimeout(function () {
-        console.log('Rendered handler for "' + state.path + '" path');
+    simulateDataFetching(function () {
+        console.log('Got data for "' + state.path + '" and rendering it\'s handler');
         React.render(React.createElement(Handler), document.getElementById('app'));
-    }, 500);
+    });
 });
